4c5e58e8bb29cae8358a11ca3e47ad7c35502c0e
lysenkonike <lysenkonike@gmail.com>
2016-12-06 21:46:00 +0300

    add http get category list

--- src/client/javascripts/controllers/service_directory_controller.js
+++ src/client/javascripts/controllers/service_directory_controller.js
@@ -1,88 +1,21 @@
-ServiceDirectoryModule.controller('serviceDirectoryCtrl', ['$scope', function($scope) {
-    $scope.services = [
-        {
-            name: 'Accountancy',
-            src: '/static/images/accountancy.png'
-        },
-        {
-            name: 'Associations',
-            src: '/static/images/associations.png'
-        },
-        {
-            name: 'Buy/Sell Dental Practice',
-            src: '/static/images/buy-sell.png'
-        },
-        {
-            name: 'Clinical Waste Collection',
-            src: '/static/images/clinical-waste.png'
-        },
-        {
-            name: 'Computer Service',
-            src: '/static/images/computer-service.png'
-        },
-        {
-            name: 'CPR/Basic Life Support Training',
-            src: '/static/images/basic-life.png'
-        },
-        {
-            name: 'Dental Recruitment',
-            src: '/static/images/dental-recruitment.png'
-        },
-        {
-            name: 'Dental Software',
-            src: '/static/images/dental-software.png'
-        },
-        {
-            name: 'Electrical Safety And Testing',
-            src: '/static/images/electrical.png'
-        },
-        {
-            name: 'Marketing',
-            src: '/static/images/marketing.png'
-        },
-        {
-            name: 'Others',
-            src: '/static/images/others.png'
-        },
-        {
-            name: 'Repairs',
-            src: '/static/images/repairs.png'
-        },
-        {
-            name: 'Solicitors',
-            src: '/static/images/solicitors.png'
-        },
-        {
-            name: 'Surgery Design',
-            src: '/static/images/surgery-design.png'
-        },
-        {
-            name: 'Training & Courses',
-            src: '/static/images/courses.png'
-        },
-        {
-            name: 'Web Designers',
-            src: '/static/images/web-designers.png'
-        },
-        {
-            name: 'Cleaning Agencies',
-            src: '/static/images/cleaning-agencies.png'
-        },
-        {
-            name: 'Dental  Chair Recovery',
-            src: '/static/images/chair-recovery.png'
-        },
-        {
-            name: 'Equipment Engineering',
-            src: '/static/images/equipment.png'
-        },
-        {
-            name: 'Selling Gold',
-            src: '/static/images/selling-gold.png'
-        },
-        {
-            name: 'Uniform & Workwear',
-            src: '/static/images/uniform.png'
+ServiceDirectoryModule.controller('serviceDirectoryCtrl', ['$scope', '$http', function($scope, $http) {
+    $scope.services = [];
+
+    var req = {
+        method: "GET",
+        url: "http://504080.com/api/v1/services/categories",
+        headers: {
+            'Authorization': '5f2fd62aa9f2d43beaefde9fd42b401febec6d56'
         }
-    ];
+    };
+
+    $http(req)
+        .success(function(obj) {
+            if (obj.success === true) {
+                $scope.services = obj.data;
+            };
+        })
+        .error(function(data, status) {
+            console.log('Error!');
+        });
 }]);
