c544c2eb398cb2ee5c00fdd69fa5facd4dac717e
lysenkonike <lysenkonike@gmail.com>
2016-12-07 17:42:36 +0300

    add optimization

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,40 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     concat = require('gulp-concat'),
-    uglify = require('gulp-uglifyjs');
+    uglifyJS = require('gulp-uglifyjs'),
+    cleanCSS = require('gulp-clean-css');
+var jsSrc = [
+    'node_modules/angular/angular.js',
+    'src/client/javascripts/app.js',
+    'src/client/javascripts/controllers/get_user_controller.js',
+    'src/client/javascripts/controllers/service_directory_controller.js',
+    'src/client/javascripts/directives/serch_company_directive.js'
+];
 
 gulp.task('build:css', function() {
-    gulp.src('src/client/stylesheets/style.sass').pipe(sass()).pipe(gulp.dest('static/stylesheets/'));
+    gulp.src('src/client/stylesheets/style.sass')
+    .pipe(sass())
+    .pipe(gulp.dest('static/stylesheets/'));
+});
+
+gulp.task('clean:css', function () {
+    gulp.src('src/client/stylesheets/style.sass')
+    .pipe(sass())
+    .pipe(gulp.dest('static/stylesheets/'))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('static/stylesheets/'));
 });
 
 gulp.task('build:js', function() {
-    return gulp.src([
-            'node_modules/angular/angular.js',
-            'src/client/javascripts/app.js',
-            'src/client/javascripts/controllers/get_user_controller.js',
-            'src/client/javascripts/controllers/service_directory_controller.js',
-            'src/client/javascripts/directives/serch_company_directive.js'
-        ])
+    return gulp.src(jsSrc)
         .pipe(concat('index.js'))
         .pipe(gulp.dest('static/javascripts/'));
 });
 
 gulp.task('uglify:js', function() {
-    return gulp.src([
-            'node_modules/angular/angular.js',
-            'src/client/javascripts/app.js',
-            'src/client/javascripts/controllers/get_user_controller.js',
-            'src/client/javascripts/controllers/service_directory_controller.js',
-            'src/client/javascripts/directives/serch_company_directive.js'
-        ])
+    return gulp.src(jsSrc)
         .pipe(concat('index.js'))
-        .pipe(uglify('index.min.js'))
+        .pipe(uglifyJS())
         .pipe(gulp.dest('static/javascripts/'));
 });
 
