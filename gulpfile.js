7862396c9bbaeff0591368adeca7138ab57dfc12
lysenkonike <lysenkonike@gmail.com>
2016-12-07 15:17:59 +0300

    make refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -19,6 +19,19 @@ gulp.task('build:js', function() {
         .pipe(gulp.dest('static/javascripts/'));
 });
 
+gulp.task('uglify:js', function() {
+    return gulp.src([
+            'node_modules/angular/angular.js',
+            'src/client/javascripts/app.js',
+            'src/client/javascripts/controllers/get_user_controller.js',
+            'src/client/javascripts/controllers/service_directory_controller.js',
+            'src/client/javascripts/directives/serch_company_directive.js'
+        ])
+        .pipe(concat('index.js'))
+        .pipe(uglify('index.min.js'))
+        .pipe(gulp.dest('static/javascripts/'));
+});
+
 gulp.task('watch:css', function() {
     gulp.watch('src/client/stylesheets/*.sass', ['build:css']);
 });
